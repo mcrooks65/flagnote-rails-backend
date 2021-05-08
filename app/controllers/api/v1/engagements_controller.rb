3f839f3bec81f018b5dfdd219d29e866c497de6e
mcrooks65 <mfcrooks@gmail.com>
2021-05-07 17:09:52 -0700

    Build out engagements controller

--- app/controllers/api/v1/engagements_controller.rb
+++ app/controllers/api/v1/engagements_controller.rb
@@ -1,2 +1,31 @@
 class Api::V1::EngagementsController < ApplicationController
+    def index
+        @engagements = Engagement.all
+        render json: @engagements
+    end
+
+    def create
+        @engagement = Engagement.new(engagement_params)
+        if @engagement.save
+            render json: @engagement
+        else
+            render json: {error: 'Error creating engagement!!11one'}
+        end
+    end
+
+    def show
+        @engagement = Engagement.find(params[:id])
+        render json: @engagement
+    end
+
+    def destroy
+        @engagement = Engagement.find(params[:id])
+        @engagement.destroy
+    end
+
+    private
+
+    def engagement_params
+        params.require(:engagement).permit(:name, :status)
+    end
 end
