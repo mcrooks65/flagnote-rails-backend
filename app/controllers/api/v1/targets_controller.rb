b18e4e26c833d15f4e0d0c8d92724421702ce838
mcrooks65 <mfcrooks@gmail.com>
2021-05-07 17:10:33 -0700

    Build out targets controller

--- app/controllers/api/v1/targets_controller.rb
+++ app/controllers/api/v1/targets_controller.rb
@@ -1,2 +1,31 @@
 class Api::V1::TargetsController < ApplicationController
+    def index
+        @targets = Target.all
+        render json: @targets
+    end
+
+    def create
+        @target = Target.new(target_params)
+        if @target.save
+            render json: @target
+        else
+            render json: {error: 'Error creating target!!11one'}
+        end
+    end
+
+    def show
+        @target = Target.find(params[:id])
+        render json: @target
+    end
+
+    def destroy
+        @target = Target.find(params[:id])
+        @target.destroy
+    end
+
+    private
+
+    def target_params
+        params.require(:target).permit(:engagement_id, :ipaddress, :hostname, :sysinfo, :vulns, :log, :loot, :status)
+    end
 end
