44908df6f8a2c0d1f0d9b7754331f5a21c6e490a
mcrooks65 <mfcrooks@gmail.com>
2021-05-28 17:24:25 -0700

    Nest targets resource inside engagements

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :targets
-      resources :engagements
+      resources :engagements do
+        resources :targets
+      end
     end
   end
 end
