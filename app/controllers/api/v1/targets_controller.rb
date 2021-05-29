d1a608e68026db791df99adef13c75cad7e4b902
mcrooks65 <mfcrooks@gmail.com>
2021-05-28 17:26:15 -0700

    Update create & add set_engagement in targets_controller.rb

--- app/controllers/api/v1/targets_controller.rb
+++ app/controllers/api/v1/targets_controller.rb
@@ -1,13 +1,16 @@
 class Api::V1::TargetsController < ApplicationController
+
+    before_action :set_engagement
+
     def index
         @targets = Target.all
         render json: @targets
     end
 
     def create
-        @target = Target.new(target_params)
+        @target = @engagement.targets.new(target_params)
         if @target.save
-            render json: @target
+            render json: @engagement
         else
             render json: {error: 'Error creating target!!11one'}
         end
@@ -24,6 +27,11 @@ class Api::V1::TargetsController < ApplicationController
     end
 
     private
+
+    def set_engagement
+        @engagement = Engagement.find(params[:engagement_id])
+    end
+
 
     def target_params
         params.require(:target).permit(:engagement_id, :ipaddress, :hostname, :sysinfo, :vulns, :log, :loot, :status)
